8b50f594bea609385819b40d54cec83f47636d24
jholloway <jholloway@griddynamics.com>
2023-06-15 15:07:44 -0500

    create Schema for item model

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category", required: true }],
+  price: { type: Number, required: true },
+  status: { type: String, required: true },
+  stock_number: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/catalog/item/${this._id}`;
+});
