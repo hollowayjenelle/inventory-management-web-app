ff1f0fbbffb9d3941642215300ebce4f24dd4a04
jholloway <jholloway@griddynamics.com>
2023-06-16 10:45:08 -0500

    add items_details, items_create_get and items_create_post functions to itemController

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,9 +1,43 @@
 const Item = require("./models/item");
 const Category = require("./models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResults } = require("express-validator");
 
 //Gets all items - no matter the category
 exports.items_list = asyncHandler(async (req, res, next) => {
-  const allItems = Item.find().sort({ name: 1 }).populate("category").exec();
+  const allItems = await Item.find()
+    .sort({ name: 1 })
+    .populate("category")
+    .exec();
   res.render("items_list", { title: "All Items", items: allItems });
 });
+
+//Get item details
+exports.items_details = asyncHandler(async (req, res, next) => {
+  const item = await Item.find(req.params.id).populate("category").exec();
+  if (item === null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    next(err);
+  }
+
+  res.render("item_detail", { title: item.name, item: item });
+});
+
+//Create item form - get
+exports.items_create_get = asyncHandler(async (req, res, next) => {
+  const allCategory = Category.find().sort({ name: 1 }).exec();
+  res.render("item_form", { title: "Create Item", categories: allCategory });
+});
+
+exports.items_create_post = [
+  body("name", "Item name is required").trim().isLength({ min: 3 }).escape(),
+  body("description", "Item description is required")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("category.*").escape(),
+  body("price", "Price is required").trim().isNumeric().escape(),
+  body("status").escape(),
+  body("stock_number", "Stock number is required").trim().isNumeric().escape(),
+];
