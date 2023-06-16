43026234f71267919a7ad1280591d6411045f29e
jholloway <jholloway@griddynamics.com>
2023-06-16 10:48:53 -0500

    create size model

---
+++ models/size.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const SizeSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+SizeSchema.virtual("url").get(function () {
+  return `catalog/size/${this._id}`;
+});
