234161e78582f6ff84e45984696c8a936a69cb0d
jholloway <jholloway@griddynamics.com>
2023-06-16 15:54:17 -0500

    create category_list view

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
-  const allCategories = Category.find().sort({ name: 1 }).exec();
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
   res.render("category_list", {
     title: "Categories",
     categories: allCategories,
